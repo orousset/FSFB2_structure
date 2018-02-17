3487bbd7015797df250ee747500eed678876ab68
olivier rousset <orousset@hotmail.com>
2018-02-17 10:59:13 +0800

    Add project files.

---
+++ FSFB2_structure/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSFB2_structure
+{
+    public class Class1
+    {
+    }
+}
